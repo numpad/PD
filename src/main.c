8cf24d3dc2a8ee4cd1f1212101979c9c42b4e25f
numpad <christian.schael87@gmail.com>
2016-02-05 11:33:52 +0100

    more qw_image tests

--- src/main.c
+++ src/main.c
@@ -12,18 +12,25 @@ int main(int argc, char *argv[]) {
 		qw_fill(100, 120, 200);
 		qw_color(200, 100, 120, 255);
 		qw_fillrect(qw_mousex - 20, qw_mousey - 20, 40, 40);
-
+		
 		qw_drawimage(background);
+		
+		if (qw_mousedown(SDL_BUTTON_LEFT))
+			qw_placeimage(background, qw_mousex, qw_mousey);
+
+		if (qw_keydown(QW_KEY(RIGHT)))
+			qw_moveimage(background, 3, 0);
+		if (qw_keydown(QW_KEY(LEFT)))
+			qw_moveimage(background, -3, 0);
 
 		qw_redraw();
-		
 		if (qw_keydown(QW_KEY(ESCAPE))) {
 			qw_quit();
 		}
 	}
 	
 	qw_destroyimage(background);
-
+	
 	return 0;
 }
 
