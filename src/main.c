6e339bed39d38c9ea834bee25171994e5937e42c
numpad <christian.schael87@gmail.com>
2016-02-07 12:24:12 +0100

    simple ai path following

--- src/main.c
+++ src/main.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "qw.h"
+#include "path.h"
 
-/* Lädt wegpunkte für Gegner, setzt wert von count = anzahl von punkten */
-SDL_Point *load_waypoints(const char *level_folder, int *count) {
-	/* Lädt /path.csv aus dem level ordner */
-	char file[128];
-	strcpy(file, level_folder);
-	strcat(file, "/path.csv");
-	
-	/* Öffnet datei und prüft auf erfolg */
-	FILE *fp = fopen(file, "r");
-	if (!fp) {
-		printf("Failed to open '%s'\n", file);
-		return NULL;
+typedef struct {
+	int x, y;
+	int waypoint;
+} enemy;
+
+enemy enemy_new(ai_path wps) {
+	return (enemy) {
+		.x = wps.points[0].x,
+		.y = wps.points[0].y,
+		.waypoint = 0
+	};
+}
+
+void enemy_draw(enemy *enemy) {
+	qw_color(120, 100, 220, 255);
+	qw_drawrect(enemy->x - 15, enemy->y - 15, 31, 31);
+	qw_drawline(enemy->x - 15, enemy->y - 15, enemy->x + 15, enemy->y + 15);
+	qw_drawline(enemy->x + 15, enemy->y - 15, enemy->x - 15, enemy->y + 15);
+}
+
+void enemy_move(enemy *enemy, ai_path wps) {
+	if (enemy->waypoint < wps.points_count - 1) {
+		enemy->waypoint += 1;
+		enemy->x = wps.points[enemy->waypoint].x;
+		enemy->y = wps.points[enemy->waypoint].y;
 	}
-	
-	/* 1. Zeile der datei: anzahl von punkten */
-	fscanf(fp, "%d\n", count);
-	
-	/* Reserviert genügend speicherplatz für alle punkte (keine überprüfung ob anzahl korrekt ist!) */
-	SDL_Point *points = malloc(sizeof(SDL_Point) * *count);
-
-	/* Lädt x,y koordinaten */
-	for (int i = 0; i < *count; ++i) {
-		int xp, yp;
-		fscanf(fp, "%d,%d\n", &xp, &yp);
-		points[i] = (SDL_Point){xp, yp};
-	}
-
-	fclose(fp);
-
-	return points;
 }
 
 int main(int argc, char *argv[]) {
@@ -39,22 +36,29 @@ int main(int argc, char *argv[]) {
 	
 	/* lädt hintergrund und wegpunkte TODO: level struct */
 	qw_image background = qw_loadimage("assets/levels/level_1/background.png");
-	int waypoints_count;
-	SDL_Point *waypoints = load_waypoints("assets/levels/level_1", &waypoints_count);
+	ai_path waypoints = load_waypoints("assets/levels/level_1");
 	
+	enemy e = enemy_new(waypoints);
+
 	while (qw_running()) {
 		qw_drawimage(background);
 		
 		qw_color(200, 100, 120, 255);
-		SDL_RenderDrawLines(qw_renderer, waypoints, waypoints_count);
+		SDL_RenderDrawLines(qw_renderer, waypoints.points, waypoints.points_count);
 		
+		if (qw_keydown(QW_KEY(RIGHT))) {
+			enemy_move(&e, waypoints);
+		}
+		
+		enemy_draw(&e);
+
 		qw_redraw();
 		if (qw_keydown(QW_KEY(ESCAPE))) {
 			qw_quit();
 		}
 	}
 	
-	free(waypoints);
+	delete_waypoints(waypoints);
 	qw_destroyimage(background);
 	
 	return 0;
---
+++ src/path.h
@@ -0,0 +1,55 @@
+#ifndef PATH_H
+#define PATH_H
+
+typedef struct {
+	SDL_Point *points;
+	int points_count;
+} ai_path;
+
+/* Gibt alle resourcen von ai_path frei */
+void delete_waypoints(ai_path ap) {
+	free(ap.points);
+	ap.points_count = 0;
+}
+
+/* Lädt wegpunkte für Gegner, setzt wert von count = anzahl von punkten */
+ai_path load_waypoints(const char *level_folder) {
+	int count;
+
+	/* Lädt /path.csv aus dem level ordner */
+	char file[128];
+	strcpy(file, level_folder);
+	strcat(file, "/path.csv");
+	
+	/* Öffnet datei und prüft auf erfolg */
+	FILE *fp = fopen(file, "r");
+	if (!fp) {
+		printf("Failed to open '%s'\n", file);
+		return (ai_path) {
+			.points = NULL,
+			.points_count = -1
+		};
+	}
+	
+	/* 1. Zeile der datei: anzahl von punkten */
+	fscanf(fp, "%d\n", &count);
+
+	/* Reserviert genügend speicherplatz für alle punkte (keine überprüfung ob anzahl korrekt ist!) */
+	ai_path path = (ai_path) {
+		.points_count = count,
+		.points = malloc(sizeof(SDL_Point) * count)
+	};
+
+	/* Lädt x,y koordinaten */
+	for (int i = 0; i < count; ++i) {
+		int xp, yp;
+		fscanf(fp, "%d,%d\n", &xp, &yp);
+		path.points[i] = (SDL_Point){xp, yp};
+	}
+
+	fclose(fp);
+
+	return path;
+}
+
+#endif
